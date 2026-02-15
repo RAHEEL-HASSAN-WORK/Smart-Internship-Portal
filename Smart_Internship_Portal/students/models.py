deb5165ce94d133122d5b1daac6007e0fe18714b
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-02-15 22:15:28 +0500

    student model updated

--- Smart_Internship_Portal/students/models.py
+++ Smart_Internship_Portal/students/models.py
@@ -7,6 +7,7 @@ from accounts.models import User
 
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     skills = models.TextField()
     resume = models.FileField(upload_to='resumes/', blank=True, null=True)
     education = models.TextField(blank=True)
